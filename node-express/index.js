9f24a805270add87dae4e83879930fb80130b256
mohsen <mohsen.abedelaal@gmail.com>
2021-04-11 15:39:15 +0300

    node js router in express

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
+//mounting the /dishes to the router
+app.use('/dishes',dishRouter);
+
+
+// app.get('/dishes/:dishId',(req,res,next)=>{
+//     res.end('Will send details for the dish:'+req.params.dishId+'to you!');
+// });
+
+// app.post('/dishes/:dishId' , (req , res,next)=>{
+
+//    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+
+// });
+
+// app.put('/dishes/:dishId' , (req , res,next)=>{
+//     res.write('Update the dish: '+ req.params.dishId);
+//     res.end('Will update the dish: '+ req.body.name + 'with details' + req.body.description);
+//  });
+
+//  app.delete('/dishes/:dishId',(req,res,next)=>{
+//      res.end('delet dish'+ req.params.dishId);
+//  });
 //serve the static files
 app.use(express.static(__dirname+'/public'));
 
